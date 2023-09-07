6c721b58a132ba6f1c301d7631086c02966fb0ca
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2023-09-07 15:24:16 +0900

    Move setupWindows to common

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,8 +1,33 @@
+import "dart:io";
+
 import "package:flutter/material.dart";
 import "package:flutter/scheduler.dart";
+import "package:flutter/foundation.dart";
+import "package:window_size/window_size.dart";
 
 const ThemeMode themeMode = ThemeMode.system;
 final bool isDarkMode =
     SchedulerBinding.instance.platformDispatcher.platformBrightness ==
         Brightness.dark;
 final appTheme = ThemeData(useMaterial3: true, colorSchemeSeed: Colors.orange);
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindows() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle("Provider Demo");
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(
+        Rect.fromCenter(
+          center: screen!.frame.center,
+          width: windowWidth,
+          height: windowHeight,
+        ),
+      );
+    });
+  }
+}
