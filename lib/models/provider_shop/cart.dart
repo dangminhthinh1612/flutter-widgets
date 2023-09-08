ab79cbc85f5184cac45472d5a85ad9ef537a4da1
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2023-09-08 10:49:19 +0900

    Add model

---
+++ lib/models/provider_shop/cart.dart
@@ -0,0 +1,26 @@
+import "package:flutter/foundation.dart";
+import "package:flutter_application_1/models/provider_shop/catalog.dart";
+import "package:flutter_application_1/models/provider_shop/item.dart";
+
+class Cart extends ChangeNotifier {
+  late Catalog _catalog;
+  final List<int> _itemIds = [];
+
+  Catalog get catalog => _catalog;
+
+  set catalog(Catalog newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  int get getPrice => items.fold(0, (total, current) => total + current.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
---
+++ lib/models/provider_shop/item.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+@immutable
+class Item {
+  final int id;
+  final String name;
+  final int price = 42;
+
+  const Item(this.id, this.name);
+
+  @override
+  int get hashCode => id;
+
+  @override
+  bool operator ==(Object other) => other is Item && other.id == id;
+}
