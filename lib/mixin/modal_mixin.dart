cdbcd6d0fa0a2708110498a5230afb4b3b08edbd
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2023-09-13 11:10:52 +0900

    Update widgetList and file name

--- lib/mixin/modal_mixin.dart
+++ lib/mixin/modal_mixin.dart
@@ -1,14 +1,11 @@
 import "package:flutter/material.dart";
 
 mixin ModalMixin {
-  List<Widget> get listWidgets;
-
-  void showModal(BuildContext context) {
+  void showModal(BuildContext context, List<Widget> listWidgets) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
         return SizedBox(
-          height: 500,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
