3829d81f14adb5c7ca55733b523411a2cdecc6eb
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2023-09-13 17:26:49 +0900

    Update default categories

--- lib/models/category_notifier.dart
+++ lib/models/category_notifier.dart
@@ -6,42 +6,66 @@ class CategoryNotifier extends ChangeNotifier {
     const Category(
       id: 1,
       name: "Ăn uống",
-      iconWidget: Icon(Icons.fastfood_rounded),
+      iconWidget: Icon(
+        Icons.fastfood_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 2,
       name: "Mua sắm",
-      iconWidget: Icon(Icons.shopping_cart_rounded),
+      iconWidget: Icon(
+        Icons.shopping_cart_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 3,
       name: "Di chuyển",
-      iconWidget: Icon(Icons.commute_rounded),
+      iconWidget: Icon(
+        Icons.commute_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 4,
       name: "Nhà cửa",
-      iconWidget: Icon(Icons.cottage_rounded),
+      iconWidget: Icon(
+        Icons.cottage_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 5,
       name: "Giải trí",
-      iconWidget: Icon(Icons.sports_esports_rounded),
+      iconWidget: Icon(
+        Icons.sports_esports_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 6,
       name: "Y tế",
-      iconWidget: Icon(Icons.medical_information_rounded),
+      iconWidget: Icon(
+        Icons.medical_information_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 7,
       name: "Hoá đơn",
-      iconWidget: Icon(Icons.receipt_rounded),
+      iconWidget: Icon(
+        Icons.receipt_rounded,
+        size: 16,
+      ),
     ),
     const Category(
       id: 8,
       name: "Tiết kiệm",
-      iconWidget: Icon(Icons.savings_rounded),
+      iconWidget: Icon(
+        Icons.savings_rounded,
+        size: 16,
+      ),
     ),
   ];
 
